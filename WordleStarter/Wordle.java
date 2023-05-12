c65eb588821f8490271e38272ae745a6edd83e17
Gael Santos <jaffary21gball360@gmail.com>
2023-05-12 08:18:45 -0400

    Update Wordle.java

--- WordleStarter/Wordle.java
+++ WordleStarter/Wordle.java
@@ -22,12 +22,26 @@ public class Wordle {
 
     public void enterAction(String s) {
         // gw.showMessage("You have to implement this method.");
+        boolean run = isWord(s);
 
-        for (int i = 0; i < s.length(); i++) {
-            String letter = s.substring(i, i+1);
-            gw.setSquareLetter(gw.getCurrentRow(), i, letter);
+        if (run == false) {
+            if (s.length() < 5) gw.showMessage("Not enough letters");
+            else if (s.length() < 5) gw.showMessage("Not in word list");
+        } else {
+            for (int i = 0; i < s.length(); i++) {
+                String letter = s.substring(i, i+1);
+                gw.setSquareLetter(gw.getCurrentRow(), i, letter);
+            }
+            gw.setCurrentRow(gw.getCurrentRow() + 1);
         }
-        gw.setCurrentRow(gw.getCurrentRow() + 1);
+    }
+
+    private static boolean isWord(String s) {
+        String[] dictionary = WordleDictionary.FIVE_LETTER_WORDS;
+        for (int i = 0; i < dictionary.length; i++) {
+            if (s.equals(dictionary[i])) return true;
+        }
+        return false;
     }
 
 /* Startup code */

2a6dee4717aadaddb76ebdbb913d46834b6dab76
Gael Santos <jaffary21gball360@gmail.com>
2023-05-15 08:45:01 -0400

    Update Wordle.java

--- WordleStarter/Wordle.java
+++ WordleStarter/Wordle.java
@@ -11,7 +11,7 @@ import edu.willamette.cs1.wordle.WordleGWindow;
 public class Wordle {
 
     public void run() {
-        correctWord = (int) (5778 * Math.random());
+        correctWord = WordleDictionary.FIVE_LETTER_WORDS[(int) (5778 * Math.random())];
         gw = new WordleGWindow();
         gw.addEnterListener((s) -> enterAction(s));
     }
@@ -24,18 +24,25 @@ public class Wordle {
     public void enterAction(String s) {
         // gw.showMessage("You have to implement this method.");
         if (s.length() > 5) s = s.substring(0,5);
-        boolean run = isWord(s.toLowerCase());
+        String word = s.toLowerCase();
+        boolean run = isWord(word);
 
         if (run == true) {
-            for (int i = 0; i < s.length(); i++) {
-                String letter = s.substring(i, i+1);
-                gw.setSquareLetter(gw.getCurrentRow(), i, letter);
+            for (int i = 0; i < word.length(); i++) {
+                String letter = word.substring(i, i+1);
+                gw.setSquareLetter(gw.getCurrentRow(), i, letter.toUpperCase());
+
+                if (correctWord.indexOf(letter) < 0) { gw.setSquareColor(gw.getCurrentRow(), i, WordleGWindow.MISSING_COLOR); }
+                else if (correctWord.indexOf(letter) != i) { gw.setSquareColor(gw.getCurrentRow(), i, WordleGWindow.PRESENT_COLOR); }
+                else { gw.setSquareColor(gw.getCurrentRow(), i, WordleGWindow.CORRECT_COLOR); }
             }
             gw.setCurrentRow(gw.getCurrentRow() + 1);
         } else {
             if (s.length() < 5) gw.showMessage("Not enough letters");
             else gw.showMessage("Not in word list");
         }
+
+        
     }
 
     private static boolean isWord(String s) {
@@ -49,13 +56,13 @@ public class Wordle {
 /* Startup code */
 
     public static void main(String[] args) {
-        System.out.println(WordleDictionary.FIVE_LETTER_WORDS.length);
         new Wordle().run();
+        
     }
 
 /* Private instance variables */
 
     private WordleGWindow gw;
-    private int correctWord;
+    private String correctWord;
 
 }

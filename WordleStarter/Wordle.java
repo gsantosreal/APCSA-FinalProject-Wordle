650ed08362a5d5a8737ec236959b7df0b4f9e34d
Gael Santos <jaffary21gball360@gmail.com>
2023-06-07 08:55:04 -0400

    update

--- WordleStarter/Wordle.java
+++ WordleStarter/Wordle.java
@@ -6,56 +6,44 @@
  */
 
 
- import edu.willamette.cs1.wordle.WordleDictionary;
- import edu.willamette.cs1.wordle.WordleGWindow;
- import java.awt.Color;
+import edu.willamette.cs1.wordle.WordleDictionary;
+import edu.willamette.cs1.wordle.WordleGWindow;
+
+public class Wordle {
+    String word;
+    String checked;
+    int r;
  
  
- public class Wordle {
-     int guesses;
-     String word;
-     String checked;
-     String alphabet = "abcdefghijklmnopqrstuvwxyz";
-     Color[] rainbow = { Color.RED, Color.ORANGE, Color.YELLOW, Color.CYAN, Color.BLUE, Color.MAGENTA };
- 
- 
-     public void run() {
-         gw = new WordleGWindow();
-         gw.addEnterListener((s) -> enterAction(s));
-         word = WordleDictionary.FIVE_LETTER_WORDS[(int) (Math.random() *
-                 WordleDictionary.FIVE_LETTER_WORDS.length)];
-         word = word.toUpperCase();
-         // for(int i = 0; i < 5; i++){
-         // gw.setSquareLetter(0, i, word.substring(i, i+1));
-         // }
-         for (int i = 0; i < alphabet.length(); i++) {
-             gw.setKeyColor(alphabet.substring(i, i + 1), Color.WHITE);
-         }
-     }
-     public void endGame(){
-         gw.setCurrentRow(8);
-     }
-     /*
-      * Called when the user hits the RETURN key or clicks the ENTER button,
-      * passing in the string of characters on the current row.
-      */
+    public void run() {
+        gw = new WordleGWindow();
+        gw.addEnterListener((s) -> enterAction(s));
+        word = WordleDictionary.FIVE_LETTER_WORDS[(int) (Math.random() * WordleDictionary.FIVE_LETTER_WORDS.length)];
+        word = word.toUpperCase();
+         
+    }
+     
+    public void endGame(){
+        gw.setCurrentRow(8);
+    }
+    /*
+    * Called when the user hits the RETURN key or clicks the ENTER button,
+    * passing in the string of characters on the current row.
+    */
  
  
      public void enterAction(String s) {
-         if (guesses >= 6) {
-            gw.showMessage("Game over! The hidden word was: " + word);
-            endGame();
-         }
         int index;
         checked = word;
-        boolean b = false;
+        boolean isWord = false;
         for (int i = 0; i < WordleDictionary.FIVE_LETTER_WORDS.length; i++) {
             if ((WordleDictionary.FIVE_LETTER_WORDS[i].equalsIgnoreCase(s))) {
-                b = true;
+                isWord = true;
             }
         }
-        if (b) {
-            guesses++;
+        
+        if (isWord) {
+            r++;
             for (int i = 0; i < 5; i++) {
                 index = checked.indexOf(s.substring(i, i + 1));
                 if (index < 0) {
@@ -83,10 +71,27 @@
                     checked = (checked.substring(0, index) + "1" + checked.substring(index + 1));
                 }
             }
+
+            // for (int i = 0; i < checked.length(); i++) {
+            //     if (checked.substring(i, i+1).equalsIgnoreCase("1")) {
+            //         gw.setSquareColor(gw.getCurrentRow(), i, gw.CORRECT_COLOR);
+            //     } else if (checked.substring(i, i+1).equalsIgnoreCase("2")) {
+            //         gw.setSquareColor(gw.getCurrentRow(), i, gw.PRESENT_COLOR);
+            //     } else {
+            //         gw.setSquareColor(gw.getCurrentRow(), i, gw.MISSING_COLOR);
+            //     }
+            // }
+
             if (checked.equals("11111")) {
-                gw.showMessage("CORRECT!");                
+                gw.showMessage("Great");                
                 endGame();
             }
+
+            if (r > 5) {
+                gw.showMessage(word);
+                endGame();
+            }
+
             gw.setCurrentRow(gw.getCurrentRow() + 1);
         } else {
             gw.showMessage("Not in word list");
